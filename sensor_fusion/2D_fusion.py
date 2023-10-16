5b49f0245766142b10ac4253171721c73470e778
Benjamin <bdevans@sun.ac.za>
2023-10-16 15:45:02 +0200

    Base 2D fusion working

---
+++ sensor_fusion/2D_fusion.py
@@ -0,0 +1,146 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from sensor_fusion.utils.Gaussian import Gaussian
+from sensor_fusion.robots.MultiSensorRover import MultiSensorRobot, GPS, IMU
+
+
+
+class SensorFusionEKF:
+    def __init__(self, init_belief, f, F_k, Q):
+        self.beliefs = [init_belief]
+        self.prior = None
+        self.f = f
+        self.F_k = F_k
+        self.Q = Q
+
+        self.sensors = []
+        self.sensor_index = {}
+
+    def add_sensor(self, sensor):
+        self.sensors.append(sensor)
+        self.sensor_index[sensor.name] = len(self.sensors)-1
+
+    def control_update(self, u):
+        mean = self.beliefs[-1].get_mean()
+        cov = self.beliefs[-1].get_covariance()
+        mean_ = self.f(mean, u)
+        F = self.F_k(mean, u)
+        cov_ = F.dot(cov).dot(F.T) + self.Q
+        prior = Gaussian(mean_, cov_)
+        self.beliefs.append(prior)
+
+    def measurement_update(self, y):
+        for m in y.keys():
+            if y[m] is None: continue
+            if not m in self.sensor_index.keys(): continue
+            idx  = self.sensor_index[m]
+            C = self.sensors[idx].C
+            R = self.sensors[idx].R
+            measurement = y[m]
+
+            mean = self.beliefs[-1].get_mean()
+            cov = self.beliefs[-1].get_covariance()
+            L = cov.dot(C.T).dot(np.linalg.inv(C.dot(cov).dot(C.T)+R))
+            mean_ = mean + L.dot(measurement-C.dot(mean))
+            cov_ = (np.eye(mean.size)-L.dot(C)).dot(cov)
+            self.beliefs[-1] = Gaussian(mean_, cov_)
+
+    def get_estimated_states(self):
+        states = [belief.get_mean() for belief in self.beliefs]
+        return np.array(states)
+    
+    def get_estimated_covariances(self):
+        covariances = [belief.get_covariance() for belief in self.beliefs]
+        return np.array(covariances)
+    
+
+simulation_time = 20 # seconds
+f_control = 20 # seconds
+f_imu = 10 # seconds
+f_gps = 0.5 # seconds
+
+
+def test_full_fusion():
+    Q = np.diag([0.2**2, 0.2**2, 0.05**2])
+    # np.random.seed(10)
+    init_state = np.array([0,0,-np.pi/4])
+    # gps = GPS(np.array([[1, 0]]), np.diag([0.1**2]), f_gps)
+    # imu = IMU(np.array([[0, 1]]), np.diag([0.5**2]), f_imu)
+    car = MultiSensorRobot(init_state, Q, 1/f_control, [])
+    init_belief = Gaussian(init_state, np.diag([1**2, 1**2, 0.5**2]))
+
+    ekf_no_sensor = SensorFusionEKF(init_belief, car.f, car.F_k, Q)
+    # lkf_imu_only = SensorFusionLKF(init_belief, car.A, car.B, car.motion_q)
+    # lkf_imu_only.add_sensor(imu)
+    # lkf_gps_only = SensorFusionLKF(init_belief, car.A, car.B, car.motion_q)
+    # lkf_gps_only.add_sensor(gps)
+    # lkf_full_fusion = SensorFusionLKF(init_belief, car.A, car.B, car.motion_q)
+    # lkf_full_fusion.add_sensor(imu)
+    # lkf_full_fusion.add_sensor(gps)
+
+    controls = np.sin(np.linspace(0, simulation_time, simulation_time*f_control) * 0.2)  *0.1
+    # controls = np.sin(np.arange(0, T*f_s+ 1, 1/f_s) * 0.2)  *0.08
+
+    for t in range(simulation_time * f_control):
+        car.move(controls[t])
+        ekf_no_sensor.control_update(controls[t])
+        # lkf_imu_only.control_update(inputs[t])
+        # lkf_gps_only.control_update(inputs[t])
+        # lkf_full_fusion.control_update(inputs[t])
+
+        measurement = car.measure()
+        ekf_no_sensor.measurement_update(measurement)
+        # lkf_imu_only.measurement_update(measurement)
+        # lkf_gps_only.measurement_update(measurement)
+        # lkf_full_fusion.measurement_update(measurement)
+
+    car_states = np.array(car.true_states)
+    print(f"Mean Error (no sensor): {np.mean(np.abs(ekf_no_sensor.get_estimated_states() - car_states)):.3f}")
+    # print(f"Mean Error (imu only): {np.mean(np.abs(lkf_imu_only.get_estimated_states() - car_states)):.3f}")
+    # print(f"Mean Error (gps only): {np.mean(np.abs(lkf_gps_only.get_estimated_states() - car_states)):.3f}")
+    # print(f"Mean Error (full fusion): {np.mean(np.abs(lkf_full_fusion.get_estimated_states() - car_states)):.3f}")
+
+    plot_2D_comparison([ekf_no_sensor], car, "comparison")
+
+
+
+def plot_2D_comparison(filter_list, car, label):
+    plt.figure(figsize=(12, 8))
+    a1 = plt.subplot(2, 2, 1)
+    a2 = plt.subplot(2, 2, 3)
+    a3 = plt.subplot(2, 2, 2)
+    a4 = plt.subplot(2, 2, 4)
+
+    labels = ["No Sensor", "IMU Only", "GPS Only", "Full Fusion"]
+
+    for f, filter in enumerate(filter_list):
+        states = filter.get_estimated_states()
+        covariances = filter.get_estimated_covariances()
+
+        a1.plot(states[:, 0], states[:, 1], label=labels[f])
+        a2.plot(states[:, 2])
+
+        x_cov = covariances[:, 0, 0]
+        y_cov = covariances[:, 1, 1]
+        pos_cov = (x_cov**2 + y_cov**2)**0.5
+        a3.plot(pos_cov**0.5, label=labels[f])
+        a4.plot(covariances[:, 2, 2]**0.5)
+
+    a1.grid(True)
+    a2.grid(True)
+    a3.grid(True)
+    a4.grid(True)
+    a1.set_ylabel("Position")
+    a2.set_ylabel("Heading")
+    a3.set_ylabel("Position Covariance")
+    a4.set_ylabel("Heading Covariance")
+    true_states = np.array(car.true_states)
+    a1.plot(true_states[:, 0], true_states[:, 1], label="True Position", color='k')
+    a1.legend()
+    a3.legend()
+
+    plt.savefig(f"media/1D_car_{label}.svg")
+
+
+test_full_fusion()
+
--- sensor_fusion/robots/MultiSensorRover.py
+++ sensor_fusion/robots/MultiSensorRover.py
@@ -7,36 +7,23 @@ RANGE = 100
 
 
 class MultiSensorRobot:
-    def __init__(self, init_state, Q, R, T_s):
+    def __init__(self, init_state, Q, T_s, sensors):
         self.deadreckon_states = [init_state]
         self.true_states = [init_state]
         self.Q = Q
-        self.R = R
+        self.sensors = sensors
         self.T_s = T_s
-        self.beacon = np.array([20, 15])
         self.measurements = []
         self.measure() # correct number.
 
     def move(self, u):
-        T_s = self.T_s
-        deadreckon_state = dynamics(self.deadreckon_states[-1][None, :], u, T_s)[0, :]
+        deadreckon_state = dynamics(self.deadreckon_states[-1][None, :], u, self.T_s)[0, :]
         self.deadreckon_states.append(deadreckon_state)
 
-        true_state = dynamics(self.true_states[-1][None, :], u, T_s)[0, :]
+        true_state = dynamics(self.true_states[-1][None, :], u, self.T_s)[0, :]
         true_state += np.random.multivariate_normal(np.zeros(3),self.Q)
         self.true_states.append(true_state)
 
-    def measure(self):
-        x_B, y_B = self.beacon
-        x, y, theta = self.true_states[-1]
-        r = np.sqrt((x-x_B)**2+(y-y_B)**2)
-        beta = np.arctan2(y-y_B,x-x_B)-theta
-        y = np.array([r, beta]) 
-        measurement = y + np.random.multivariate_normal(np.zeros(2),self.R)
-        self.measurements.append(measurement)
-
-        return measurement
-        
     def get_states(self):
         return np.array(self.true_states), np.array(self.deadreckon_states)
     
@@ -46,42 +33,20 @@ class MultiSensorRobot:
         else:
             new_state = dynamics(state, u, self.T_s)
         return new_state
-    
-    def h(self, states):
-        x_B, y_B = self.beacon
-        if len(states.shape) == 1:
-            states = states[None, :]
-        x, y, theta = np.hsplit(states, 3)
-        r = np.sqrt((x-x_B)**2+(y-y_B)**2)
-        beta = np.arctan2(y-y_B,x-x_B)-theta
-        y = np.concatenate([r, beta], axis=1)
-        if y.shape[0] == 1:
-            y = y[0]
-
-        return y
-    
+        
     def F_k(self, state, u):
-        T_s = self.T_s
-        x, y, theta = state
         F = np.eye(3)
-        F[0][2] = -T_s*SPEED*np.sin(theta)
-        F[1][2] = T_s*SPEED*np.cos(theta)
+        F[0][2] = -self.T_s*SPEED*np.sin(state[2])
+        F[1][2] = self.T_s*SPEED*np.cos(state[2])
         return F
     
-    def H_k(self, state):
-        x, y, theta = state
-        delta_x = x - self.beacon[0]
-        delta_y = y - self.beacon[1]
-        r = np.sqrt(delta_x**2+delta_y**2)
-        H = np.zeros((2,3))
-        H[0][0] = delta_x/r
-        H[0][1] = delta_y/r
-        # H[1][0] = 1/(delta_x+delta_y**2/delta_x)
-        H[1][0] = -delta_y/(delta_x**2+delta_y**2)
-        # H[1][1] = -1/(delta_y+delta_x**2/delta_y)
-        H[1][1] = delta_x/(delta_y**2+delta_x**2)
-        H[1][2] = -1
-        return H
+    def measure(self):
+        measurements = {}
+        for sensor in self.sensors:
+            m = sensor.measure(self.true_states[-1], self.time)
+            measurements[sensor.name] = m
+
+        return measurements
 
 
 @njit(cache=True)
@@ -94,3 +59,29 @@ def dynamics(state, phi, T_s):
     return new_state
 
 
+class GPS:
+    def __init__(self, C, R, frequency) -> None:
+        self.name = "GPS"
+        self.C = C
+        self.R = R
+        self.dt = 1/frequency
+
+    def measure(self, state, t):
+        if t%self.dt < 0.05: 
+            measurement =  self.C.dot(state) + np.random.multivariate_normal(np.zeros(1), self.R)
+        else: measurement = None
+        return measurement
+
+
+class IMU:
+    def __init__(self, C, R, frequency) -> None:
+        self.name = "IMU"
+        self.C = C
+        self.R = R
+        self.dt = 1/frequency
+
+    def measure(self, state, t):
+        if t%self.dt < 0.05: 
+            measurement =  self.C.dot(state) + np.random.multivariate_normal(np.zeros(1), self.R)
+        else: measurement = None
+        return measurement
